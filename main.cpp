b723702fd4584f27b129977fcf8ac4f42bc468b5
omer hayoona <omer.hayoon@gmail.com>
2024-06-02 19:50:53 +0300

    new

--- main.cpp
+++ main.cpp
@@ -4,36 +4,28 @@
 int *allocateAndInput(int *size) {
     printf("Enter the initial size: ");
     scanf("%d", size);
-
     if (*size <= 0) {
         printf("Invalid size\n");
         exit(0);
     }
-
     int *numbers = (int *)calloc(*size, sizeof(int));
     if (numbers == NULL) {
         printf("Memory allocation failed\n");
         exit(0);
     }
-
     printf("Enter %d numbers:\n", *size);
     for (int i = 0; i < *size; i++) {
         scanf("%d", &numbers[i]);
     }
-
     return numbers;
 }
-
 int main() {
     int size;
     int *numbers = allocateAndInput(&size);
-
     printf("The numbers are:\n");
     for (int i = 0; i < size; i++) {
         printf("%d\n", numbers[i]);
     }
-
     free(numbers);
-
     return 0;
 }
